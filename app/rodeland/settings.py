6eb58eb2e4779b3ec5cda47408ba7e3a34af794f
Bart Vleminckx <bart.vleminckx@ilvo.vlaanderen.be>
2025-02-07 15:01:39 +0100

    Remove analytical

--- app/rodeland/settings.py
+++ app/rodeland/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
-from .deploy import DEBUG, SECRET_KEY, ALLOWED_HOSTS, GOOGLE_ANALYTICS_GTAG_PROPERTY_ID
+from .deploy import DEBUG, SECRET_KEY, ALLOWED_HOSTS
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -30,8 +30,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    'analytical',
 ]
 
 MIDDLEWARE = [
